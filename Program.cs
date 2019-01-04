ceb36f0bc9b7c13b54eebe2b59e6f5f9b3396c14
toddcoder <38821140+toddcoder@users.noreply.github.com>
2019-01-04 09:00:09 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using SQLBatch.Library;
+using Standard.Applications;
+using Standard.Computer;
+using static System.Console;
+
+namespace SQLBatch
+{
+   internal class Program : CommandLine
+   {
+      FileStream fileStream;
+      StreamWriter writer;
+
+      static void Main(string[] args)
+      {
+         var program = new Program { Test = false };
+         program.Run(args);
+      }
+
+      public override void Execute(Arguments arguments)
+      {
+         try
+         {
+            arguments.AssertMinimumCount(1);
+            if (arguments.Exists(1) && arguments[1].Text == "/out")
+            {
+               FileName outfile = arguments[2].Text;
+               if (outfile.Exists())
+                  outfile.Delete();
+               fileStream = outfile.WritingStream();
+               writer = new StreamWriter(fileStream) { AutoFlush = true };
+               SetOut(writer);
+               SetError(writer);
+            }
+            var file = arguments[0].FileName.Required($"File {arguments[0].Text} not found");
+            var chooser = new Chooser(file);
+            chooser.Choose();
+            WriteLine("Done");
+         }
+         finally
+         {
+            writer?.Close();
+            fileStream?.Close();
+         }
+      }
+   }
+}
